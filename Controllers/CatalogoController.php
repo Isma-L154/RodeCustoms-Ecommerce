7bce5a6c1cada99975ddcb06bf8a958e49596de9
Ismael Leon <ileon30419@ufide.ac.cr>
2023-11-29 00:22:54 -0600

    Revert "Revert "Merge branch 'Developer_Branch-Isma'""
    This reverts commit cdb8236a783b9ae190ca0c3300105a9cee719fac.

--- Controllers/CatalogoController.php
+++ Controllers/CatalogoController.php
@@ -16,24 +16,90 @@ switch ($_GET["op"]) {
                 }else{
                     $Ruta = '../Views/assets/Img/'.'Logo2.png';
                 }
-                $datos[] = array(
-                    "0" => $reg->getId(),
-                    "1" => $reg->getNombre(),
-                    "2" => $reg->getDescripcion(),
-                    "3" => '<img src="'. $Ruta.'" width="50px" heigth="50px"/>',                   
-                    "4" => $reg->getPrecio(),   
-                );
+                echo '<div class="col-md-3">
+                <div class="wsk-cp-product">
+                    <div class="wsk-cp-img">
+                        <img src="' . $Ruta . '" alt="Product" class="img-responsive" />
+                    </div>
+                    <div class="wsk-cp-text">
+                        <div class="category">
+                            <span>';
+                            
+            // Estructura condicional para mostrar el nombre de la categoría
+            if ($reg->getCategoria() == 1) {
+                echo 'Sudaderas';
+            } elseif ($reg->getCategoria() == 2) {
+                echo 'Camisetas';
+            } else {
+                echo 'Varios'; // Puedes agregar más condiciones según sea necesario
             }
-            $Resultado = array(
-                "sEcho" => 1, ##informacion para datatables
-                "iTotalRecords" =>count($datos), ## total de registros al datatable
-                "iTotalDisplayRecords" => count($datos), ## enviamos el total de registros a visualizar
-                "aaData" => $datos
-            );
-            echo json_encode($Resultado);
+            
+            echo '</span>
+                        </div>
+                        <div class="title-product">
+                            <h3>' . $reg->getNombre() . '</h3>
+                        </div>
+                        <div class="description-prod">
+                            <p>' . $reg->getDescripcion() . '</p>
+                            <span class="size"> S, M, L, XL </span>
+                        </div>
+                                               
+                        <div class="card-footer">
+                            <div class="wcf-left"><span class="price">' . $reg->getPrecio() . 'CRC</span></div>
+                            <div class="wcf-right"><a href="./Carrito.php" class="buy-btn"><i class="fa-solid fa-cart-shopping"></i></div></a>
+                        </div>
+                    </div>
+                </div>
+            </div>';
+            
+            }
         break;
     
+
+    case 'Filtro_Tipo':
+        $articulo = new Articulo();
+        $Tipo_Filtro = isset($_POST["tipo"]) ? trim($_POST["tipo"]) : "";
+        $Tipo_Filtro = $articulo ->FiltrarPorCategoria();
+        $datos = array();
+        if ($Tipo_Filtro > 1) {
+            foreach ($Tipo_Filtro as $filtro) {
+                if  ($filtro-> getRuta_imagen() != '' && $filtro-> getRuta_imagen() != null ) {
     
-    
+                    $Ruta = $filtro->getRuta_imagen();
+                    }else{
+                        $Ruta = '../Views/assets/Img/'.'Logo2.png';
+                    }
+                    echo '<div class="col-md-3">
+                    <div class="wsk-cp-product">
+                        <div class="wsk-cp-img">
+                            <img src="' . $Ruta . '" alt="Product" class="img-responsive" />
+                        </div>
+                        <div class="wsk-cp-text">
+                            <div class="category">
+                                <span>'. $filtro->getCategoria(). '</span>
+                            </div>
+                            <div class="title-product">
+                                <h3>' . $filtro->getNombre() . '</h3>
+                            </div>
+                            <div class="description-prod">
+                                <p>' . $filtro->getDescripcion() . '</p>
+                                <span class="size"> S, M, L, XL </span>
+                            </div>
+                                                   
+                            <div class="card-footer">
+                                <div class="wcf-left"><span class="price">' . $filtro->getPrecio() . 'CRC</span></div>
+                                <div class="wcf-right"><a href="./Carrito.php" class="buy-btn"><i class="fa-solid fa-cart-shopping"></i></div></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+                
+                }
+        
+        }else{
+           echo '<p>No se encontraron resultados para la búsqueda.</p>';
+        }
+        
+        break;    
     }
 ?>
--- Views/assets/JavaScript/Articulo.js
+++ Views/assets/JavaScript/Articulo.js
@@ -1,21 +1,65 @@
 
-function listarTodosArticulos(){
-    tabla = $('#tblcard').dataTable({    
-      aProcessing: true, //actiavmos el procesamiento de datatables
-      ajax: {
-          //URL del controlador
-          url: '../Controllers/CatalogoController.php?op=listar_articulos',
-          type: 'get',
-          dataType: 'json',
-          
-          error: function (e) {
-            console.log(e.responseText);
-          },
-          bDestroy: true,
-          iDisplayLength: 1,
+function listarTodosArticulos() {
+  // Realizar la petición AJAX para obtener los datos
+    $.ajax({
+        url: '../Controllers/CatalogoController.php?op=listar_articulos',
+        type: 'POST',
+        dataType: 'html',
+        success: function(data) {
+            // Insertar el HTML generado en el contenedor deseado
+            $('#contenedorCards').html(data);
         },
-    });  
+        error: function(e) {
+            console.log(e.responseText);
+        }
+    });
+
 }
+
+$('#filtroContent').on('submit', function (event) {
+  event.preventDefault();
+  $.ajax({
+        url: '../Controllers/CatalogoController.php?op=Filtro_Tipo',
+        type: 'POST',
+        dataType: 'html',
+        success: function(data) {
+            // Insertar el HTML generado en el contenedor deseado
+            $('#contenedorCards').html(data);
+        },
+        error: function(e) {
+            console.log(e.responseText);
+        }
+    });
+
+});
+
+//FUNCIONES PRINCIPALES PARA DISEÑOS DEL CATALOGO
 $(function(){
   listarTodosArticulos();
+});
+
+$('.buy').click(function(){
+  $('.bottom').addClass("clicked");
+});
+
+$('.remove').click(function(){
+  $('.bottom').removeClass("clicked");
+});
+
+//Funcion para desplegar los filtros de la pagina
+document.addEventListener('DOMContentLoaded', function () {
+  const btnFiltros = document.getElementById('btnFiltros');
+  const filtroContent = document.getElementById('filtroContent');
+
+  btnFiltros.addEventListener('click', function () {
+    // Alternar la visibilidad de la ventana de filtros
+    filtroContent.style.display = filtroContent.style.display === 'block' ? 'none' : 'block';
+  });
+
+  // Cierra la ventana de filtros si se hace clic fuera de ella
+  window.addEventListener('click', function (event) {
+    if (!filtroContent.contains(event.target) && event.target !== btnFiltros) {
+      filtroContent.style.display = 'none';
+    }
+  });
 });
